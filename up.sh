987dc221cac0e6a67b41c00ea70bfe1ed8b24cb2
Mike Long <mike@praqma.com>
2017-03-09 14:31:56 +0100

    Change prometheus image to cyberdojo's in the up.sh script

--- up.sh
+++ up.sh
@@ -4,5 +4,5 @@ set -e
 docker run -d -p 9090:9090 \
   --name cyber-dojo-prometheus \
   -v $PWD/prometheus.yml:/etc/prometheus/prometheus.yml \
-  prom/prometheus \
+  cyberdojo/prometheus \
   -config.file=/etc/prometheus/prometheus.yml
