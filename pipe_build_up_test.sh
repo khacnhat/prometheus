d4b24e6f5bec643429496711730ce2af6db376f8
Jon Jagger <jon@jaggersoft.com>
2018-04-29 17:33:14 +0100

    refactoring; bash quote style

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -3,4 +3,4 @@ set -e
 
 readonly SH_DIR="$( cd "$( dirname "${0}" )" && pwd )/sh"
 
-${SH_DIR}/build_docker_images.sh
+"${SH_DIR}/build_docker_images.sh"
--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -3,4 +3,6 @@ set -e
 
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 
-docker-compose --file ${ROOT_DIR}/docker-compose.yml build
+docker-compose \
+  --file "${ROOT_DIR}/docker-compose.yml" \
+  build
